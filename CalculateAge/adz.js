dd4d2b89e7d975c137f8974cf7e0663e0d98456c
Andisiwe Nkwana <andisiwenkwana9@gmail.com>
2023-04-19 17:17:15 +0200

    slider

--- CalculateAge/adz.js
+++ CalculateAge/adz.js
@@ -1,13 +1,38 @@
+  andy= []
 
-// function calcAge(){
-//     let thisYear = 2023;
-//     let bornYear = 1994;      
+  if(localStorage.getItem("A")){
+    newAge = JSON.parse(localStorage.getItem("A"));
+  }
 
-// let newAge = thisYear - bornYear
-//     return newAge;
+function calcAge(){
+     localStorage.setItem("newAge",JSON.stringify(andy))
 
-// }
-// console.log(calcAge())
+    // let thisYear = 2023;
+    let bornYear = document.getElementById("age").value
+    let newAge = new Date();
+    let year = newAge.getFullYear();
+    age = (year - bornYear);
+
+    // alert(age);
+
+    if(!bornYear){
+                 alert ("age can not be empty")
+     }else if(age < 0){
+         alert ("age can not  be nagative")
+     } else
+         alert(age);
+    
+
+    localStorage.SetItem("A",(age))
+
+    array = document.getElementById("age").value
+    andy.push(array)
+
+    localStorage.SetItem("A",JSON.stringify(andy));
+    
+}
+
+calcAge()
 
 
 
@@ -20,26 +45,35 @@
 // }
 // console.log(calcAge(2023, 1994))
 
+// andy = []
 
-function calcAge(thisYear, bornYear){
-    let num1 = document.querySelector('.first').value;
-    let num2 = document.querySelector('.second').value;
-    //let newAge = thisYear - bornYear;
-    let age = num1 - num2;
+// function calcAge(){
+//     localStorage.setItem("newAge",JSON.stringify(andy))
+
+//     let thisYear = document.querySelector('.first').value;
+//     let bornYear = document.querySelector('.second').value;
+//     let newAge = thisYear - bornYear;
+//     let age = num1 - num2;
+//     //newAge = age;
     
-    alert (age);
-    //console.log("hello world")
-
-
-    // if(newAge != -29){
-    //     return "Please enter your born year";
-
-    // }else{
-    //     return "awsome";
-        // }
-    // console.log("hello world")
-    }
- console.log(calcAge(2023, 1994))
+//     //console.log(age);
+//     if(!thisYear && bornYear){
+//         alert ("age can not be empty")
+//     }else{
+//         age = bornYear - thisYear
+//            if(age < 0){
+//             alert ("age can not  be nagative")
+         
+//            }
+//         }
+//      }
+//      localStorage.setItem(newAge)
+//     //  newAge = JSON.parse(localStorage.getItem("newAge"));
+//     newAge = document.querySelector
+  
+//     // }
+ 
+//      calcAge();
 
 
 
--- ImageSlide/script.js
+++ ImageSlide/script.js
@@ -1,20 +1,36 @@
 
 
-let show_images = document. querySelector(".show_images");
-let foward = document. querySelector(".foward");
-let prev = document. querySelector(".prev");
+let show_images = document.querySelector(".show_images");
+let foward = document.querySelector(".foward");
+let prev = document.querySelector(".prev");
 
 
-let imgs = ["image1.jpg","image2.jpg" ,"image3.jpg"]
+// let imgs = ["image1.jpg","image2.jpg" ,"image3.jpg"]
+
+// let index = 0;
+let images;
+
+if(images = localStorage.getItem("images")){
+    images = JSON.parse(localStorage.getItem("images"));
+}else{
+    images = 
+    ["https://i.postimg.cc/NjRSYSpR/image1.webp",
+     "https://i.postimg.cc/mDKKT4fN/image2.webp",
+     "https://i.postimg.cc/wj9ZqFPP/image3.jpg"];
+}
 
 let index = 0;
+// let show_images = document. querySelector(".show_images");
+// let foward = document. querySelector(".foward");
+// let prev = document. querySelector(".prev");
+
 
 function next(){
-show_images.src = imgs[index]
+show_images.src = images[index]
     // let show = imgs[index];
     // console.log(show);
     index++;
-    if(index >= imgs.length){
+    if(index >= images.length){
         index=0;
     }
 
@@ -23,35 +39,43 @@ show_images.src = imgs[index]
 function back(){
     // let show = image[i]
     // index = index-1;
-    show_images.src = imgs[index]
-    index--
+    show_images.src = images[index]
+    index--;
 
     if(index < 0){
-        index = imgs.length-1;
+        index = images.length-1;
+        index=0;
     }
 }
 
-
-
-let img = document.getElementById("image")
-//let img = document.getElementById("image").value 
-let current = 0;
-
 function add(){
 
-    let img = document.getElementById("image").value
-    Image.push("image") 
+    let imgs = document.getElementById("img").value;
+    //console.log(images)
+    //images.push(imgs);
+    //localStorage.setItem("images",JSON.stringify(images));
+    let check = images.some(ele => ele == imgs)
+
+    if(!imgs){
+        alert ("put something") 
+
+    }else if(check ){
+        alert ("this image already exist")
+    }else{    
+    
+    images.push(imgs)
+    localStorage.setItem("images",JSON.stringify(images));
+    console.log(images)
+}
 }
 
-
-
-
-let pictures = ["image1.jpg","image2.jpg","image3.jpg"]
-
- localStorage.setItem("images",JSON.stringify(pictures));
- //localStorage.getItem(pictures)
-
-
+function remove(){
+    //let images = document.getElementById("img").value;
+  images.splice(index,1);
+  localStorage.setItem("images",JSON.stringify(images));
+}
+ 
+ 
 
 
 
